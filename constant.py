938340bd37ec48c572d38423009e33d655942635
Matteo Turri <matteo.turri@buildo.io>
2022-03-02 19:49:10 +0100

    New box_id and base_url

--- constant.py
+++ constant.py
@@ -5,6 +5,7 @@ INPUT_DATETIME_FORMAT = '%Y-%m-%d_%H:%M'
 CLASSES_RESULT_DATE_FORMAT = '%A %d/%m/%Y'
 HEADERS = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
            'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 6.0; Android SDK built for x86_64 Build/MASTER)'}
-BASE_URL: str = "https://cfavanguardia.shaggyowl.com/funzioniapp/v341/"
-BOX_ID = 105
-
+# BASE_URL: str = "https://cfavanguardia.shaggyowl.com/funzioniapp/v341/"
+# BOX_ID = 105
+BASE_URL: str = "https://cfcenisio.shaggyowl.com/funzioniapp/v341/"
+BOX_ID = 52
